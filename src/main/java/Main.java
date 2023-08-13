bb4eeba609bfaf5d7aa17ddcd5e074073a02bb47
Aliaksandr Komar <Komarthebest1992@gmail.com>
2023-08-13 21:03:48 +0300

    ExampleTwoPartOneForKirill

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        Runnable runnable = () -> {
-            System.out.println("hello java");};
-       runnable.run();
+        String[] arr = {"word", "mouse", "calculator"};
+        Func func;
+        func = (String [] arrayOne) -> {
+                String preMaxWord = arrayOne[0];
+                String maxWord = arrayOne[0];
+                for (int i = 0; i < arrayOne.length; i++){
+                    if (arrayOne[i].length() > maxWord.length()) {
+                        preMaxWord = maxWord;
+                        maxWord = arrayOne[i];
+                    }
+                }
+                return maxWord;
+        };
+       String str = func.preMaxWord(arr);
+        System.out.println(str);
+
     }
-}
+    }
+
