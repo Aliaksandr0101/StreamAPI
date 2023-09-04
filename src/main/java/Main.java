ea20fb902c02d522ef1f54c23fadea5a329ff1db
Aliaksandr Komar <Komarthebest1992@gmail.com>
2023-09-04 16:53:33 +0300

    ExampleThreePartOne

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -29,6 +33,16 @@ public class Main {
         boolean result = Stream.of(1, 2, 3, 4, 5)
                 .noneMatch(u -> u == 3);
         System.out.println(result);
+
+        List<String> listOne = Stream.of("a", "b", "c", "d")
+                .collect(Collectors.collectingAndThen(
+                        Collectors.toMap(Function.identity(), s -> s+s),
+        map -> map.entrySet().stream()))
+                .map(e-> e.toString())
+                .collect(Collectors.collectingAndThen(
+                        Collectors.toList(),
+                        Collections::unmodifiableList));
+        listOne.forEach(System.out::println);
 
 
         /*int j = 9;
