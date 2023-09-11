1bd028235e397c6c3bf95948463b97c1a28edaba
Aliaksandr Komar <Komarthebest1992@gmail.com>
2023-09-11 21:27:11 +0300

    ExampleThreePartFour

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,6 +9,16 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
+        List<String> phones = new ArrayList<String>();
+        Collections.addAll(phones, "iPhone X", "Nokia 9", "Huawei Nexus 6P",
+                "Samsung Galaxy S8", "LG G6", "Xiaomi MI6",
+                "ASUS Zenfone 3", "Sony Xperia Z5", "Meizu Pro 6",
+                "Pixel 2");
+
+        phones.stream()
+                .filter(p->p.length()<12)
+                .sorted() // сортировка по возрастанию
+                .forEach(s->System.out.println(s));
       /*  IntStream.concat(
                 IntStream.range(2,5),
                 IntStream.range(0,4))
