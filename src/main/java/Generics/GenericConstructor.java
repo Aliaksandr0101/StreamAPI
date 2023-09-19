1027c6cc802cee1ed7882f3c95f9499143801f69
Aliaksandr Komar <Komarthebest1992@gmail.com>
2023-09-19 21:50:14 +0300

    TaskOneGenerics

---
+++ src/main/java/Generics/GenericConstructor.java
@@ -0,0 +1,9 @@
+package Generics;
+
+public class GenericConstructor<T,V> {
+    public GenericConstructor(T elementT, V elementV) {
+        if (elementT.getClass() == elementV.getClass()){
+            
+        }
+    }
+}
