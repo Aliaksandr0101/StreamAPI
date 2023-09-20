f68a8a74b14857d5ff5d1b338bb01a6adea13102
Aliaksandr Komar <Komarthebest1992@gmail.com>
2023-09-21 00:12:35 +0300

    TaskOneGenerics

--- src/main/java/Generics/GenericConstructor.java
+++ src/main/java/Generics/GenericConstructor.java
@@ -2,7 +2,8 @@ package Generics;
 
 public class GenericConstructor<T,V> {
     public GenericConstructor(T elementT, V elementV) {
-        if (elementT instanceof V){
+        if (elementT.getClass().isInstance(elementV)){
+
             System.out.println("The classes coincided");
         } else {
             System.out.println("The classes is not match");
