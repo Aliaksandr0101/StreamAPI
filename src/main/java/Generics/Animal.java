c3c5feadd067d683686c3da476ab214ad4778098
Aliaksandr Komar <Komarthebest1992@gmail.com>
2023-09-21 13:16:29 +0300

    TaskTwoGenerics

--- src/main/java/Generics/Animal.java
+++ src/main/java/Generics/Animal.java
@@ -1,10 +1,40 @@
 package Generics;
 
 public class Animal {
-    public class Whale extends Animal{};
-    public class Dog extends Animal{};
+    private String typeAnimal;
+    private String colorAnimal;
+
+    public String getTypeAnimal() {
+        return typeAnimal;
+    }
+
+    public String getColorAnimal() {
+        return colorAnimal;
+    }
+
+    public Animal(String typeAnimal, String colorAnimal) {
+        this.typeAnimal = typeAnimal;
+        this.colorAnimal = colorAnimal;
+    }
+
+    public class Whale extends Animal{
+
+        public Whale(String typeAnimal, String colorAnimal) {
+            super(typeAnimal, colorAnimal);
+        }
+    };
+    public class Dog extends Animal{
+
+        public Dog(String typeAnimal, String colorAnimal) {
+            super(typeAnimal, colorAnimal);
+        }
+    };
+    public class AnimalGeneric<T extends Animal> {
+
+    };
+
     public static void main(String[] args) {
-        
+
     }
 }
 
