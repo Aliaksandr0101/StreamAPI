c47e4ae2695333ceb189762e97b3aad9840c2c10
Aliaksandr Komar <Komarthebest1992@gmail.com>
2023-09-21 15:27:03 +0300

    TaskTwoGenerics

--- src/main/java/Generics/Animal.java
+++ src/main/java/Generics/Animal.java
@@ -21,9 +21,26 @@ class Dog extends Animal{
     public Dog(String typeAnimal, String colorAnimal) {
         super(typeAnimal, colorAnimal);
     }
-};
+}
 class Whale extends Animal{
     public Whale(String typeAnimal, String colorAnimal) {
         super(typeAnimal, colorAnimal);
     }
-};
+}
+ class AnimalGenerics<T extends Animal>{
+     public String bringOut(T animal){
+         String infoAboutAnimal = "This is " + animal.getColorAnimal() + " " + animal.getTypeAnimal() + ".";
+         return infoAboutAnimal;
+     }
+ }
+  class MainTest{
+      public static void main(String[] args) {
+          Dog dog = new Dog("dog", "black");
+          Whale whale = new Whale("whale", "blue");
+          AnimalGenerics dogInfo = new AnimalGenerics();
+          AnimalGenerics whaleInfo = new AnimalGenerics();
+          System.out.println(dogInfo.bringOut(dog));
+          System.out.println(whaleInfo.bringOut(whale));
+      }
+
+  }
