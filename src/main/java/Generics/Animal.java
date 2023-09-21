f0af505e0cf05f14a7fb2582cad614b001bbd2ed
Aliaksandr Komar <Komarthebest1992@gmail.com>
2023-09-21 15:05:46 +0300

    TaskTwoGenerics

--- src/main/java/Generics/Animal.java
+++ src/main/java/Generics/Animal.java
@@ -1,8 +1,13 @@
 package Generics;
 
-public class Animal {
+class Animal{
     private String typeAnimal;
     private String colorAnimal;
+
+    public Animal(String typeAnimal, String colorAnimal) {
+        this.typeAnimal = typeAnimal;
+        this.colorAnimal = colorAnimal;
+    }
 
     public String getTypeAnimal() {
         return typeAnimal;
@@ -11,40 +16,14 @@ public class Animal {
     public String getColorAnimal() {
         return colorAnimal;
     }
-
-    public Animal(String typeAnimal, String colorAnimal) {
-        this.typeAnimal = typeAnimal;
-        this.colorAnimal = colorAnimal;
-    }
-
-    public static class Whale extends Animal{
-
-        public Whale(String typeAnimal, String colorAnimal) {
-            super(typeAnimal, colorAnimal);
-        }
-    };
-    public static class Dog extends Animal{
-
-        public Dog(String typeAnimal, String colorAnimal) {
-            super(typeAnimal, colorAnimal);
-        }
-    };
-    public static class AnimalGenericInfo<T extends Animal> {
-        public void BringOut(T animal) {
-            System.out.println("Animal is " + animal.getTypeAnimal() + ". The color of this " +
-                    animal.getTypeAnimal() + " " + animal.getColorAnimal() + ".");
-        }
-
-    };
-
-    public static void main(String[] args) {
-        Dog dog = new Dog("dog", "black");
-        Whale whale = new Whale("whale", "blue");
-        AnimalGenericInfo<Dog> infoDog = new AnimalGenericInfo<>();
-        AnimalGenericInfo<Whale> infoWhole = new AnimalGenericInfo<>();
-        infoDog.BringOut(dog);
-        infoWhole.BringOut(whale);
-
-    }
 }
-
+class Dog extends Animal{
+    public Dog(String typeAnimal, String colorAnimal) {
+        super(typeAnimal, colorAnimal);
+    }
+};
+class Whale extends Animal{
+    public Whale(String typeAnimal, String colorAnimal) {
+        super(typeAnimal, colorAnimal);
+    }
+};
