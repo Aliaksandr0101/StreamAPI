0b0fc1d1431396aa65033ad828aabea45e8250fe
Aliaksandr Komar <Komarthebest1992@gmail.com>
2023-09-23 19:06:23 +0300

    TaskTwoGenerics

--- src/main/java/Generics/GenericConstructor.java
+++ src/main/java/Generics/GenericConstructor.java
@@ -23,18 +23,18 @@ public class GenericConstructor<T,V> {
 
 
     }
-    static class Account<T>{
+}
+ class Account<T>{
 
-        private T id;
-        private int sum;
+    private T id;
+    private int sum;
 
-        Account(T id, int sum){
-            this.id = id;
-            this.sum = sum;
-        }
-
-        public T getId() { return id; }
-        public int getSum() { return sum; }
-        public void setSum(int sum) { this.sum = sum; }
+    Account(T id, int sum){
+        this.id = id;
+        this.sum = sum;
     }
+
+    public T getId() { return id; }
+    public int getSum() { return sum; }
+    public void setSum(int sum) { this.sum = sum; }
 }
