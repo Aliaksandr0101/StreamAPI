330ce62b1db687262409677203f537e5d7e7adde
Aliaksandr Komar <Komarthebest1992@gmail.com>
2023-09-29 17:31:34 +0300

    WorkWithFiles

--- src/main/java/FilesWork/WorkWithFile.java
+++ src/main/java/FilesWork/WorkWithFile.java
@@ -1,4 +1,8 @@
 package FilesWork;
+import com.sun.source.tree.IfTree;
+
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,6 +16,16 @@ public class WorkWithFile {
         if ((Files.exists(Path.of("Readme.txt")))){
             Files.delete(Path.of("Readme.txt"));
         }
+
+
+        if (!(Files.exists(Path.of("Readme.txt")))) {
+            Files.createFile(Path.of("Readme.txt"));
+        }
+        File file = new File("readme.txt");
+        String str = "I love java";
+        FileWriter fileWriter = new FileWriter(file);
+        fileWriter.write(str);
+        fileWriter.close();
     }
 
 }
